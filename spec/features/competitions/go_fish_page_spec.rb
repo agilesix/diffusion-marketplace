84d209eea2d0d7cf265169061c47c892df58c12c
Joshua Drumm <joshua.drumm@agile6.com>
2020-11-30 21:42:30 -0500

    dm-increase-test-coverage: Added competition page tests and merged from master

--- spec/features/competitions/go_fish_page_spec.rb
+++ spec/features/competitions/go_fish_page_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-describe 'Shark Tank landing page', type: :feature do
+describe 'Go Fish landing page', type: :feature do
   it 'should redirect the user to the homepage' do
-    visit '/competitions/shark-tank'
-    expect(page).to_not have_content('Shark Tank')
+    visit '/competitions/go-fish'
+    expect(page).to_not have_content('Go Fish')
     expect(page).to have_content('Welcome to the Diffusion Marketplace')
   end
 end
